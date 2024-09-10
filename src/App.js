2827ea3f8c37fc09d8743d0236a311d2dfb75ff9
Raj, Shanu <Shanu.Raj@tabcorp.com.au>
2024-09-10 11:52:21 +0530

    Added page animation on route change

--- src/App.js
+++ src/App.js
@@ -1,31 +1,64 @@
 import './App.scss';
-import MainLayout from './components/layouts/MainLayout'
-import { Routes, BrowserRouter, Route } from 'react-router-dom'
-import Home from './pages/Home/Home'
-import Sudoku from './pages/Sudoku/Sudoku'
-import Api from './pages/Api/Api'
-import HowToUse from './pages/HowToUse/HowToUse'
+import MainLayout from './components/layouts/MainLayout';
+import { Routes, BrowserRouter as Router, Route, useLocation } from 'react-router-dom';
+import Home from './pages/Home/Home';
+import Sudoku from './pages/Sudoku/Sudoku';
+import Api from './pages/Api/Api';
+import HowToUse from './pages/HowToUse/HowToUse';
+import Err404 from './pages/Err404/Err404';
+import GoToTop from './components/common/GoToTop';
+import { AnimatePresence, motion } from 'framer-motion';
 
-import Err404 from './pages/Err404/Err404'
+const PageWrapper = ({ children }) => (
+  <motion.div
+    // initial={{ opacity: 0, y: 10 }}
+    animate={
+      {
+        opacity: 1,
+        y: 0
+      }
+    }
+    exit={
+      {
+        opacity: 0,
+        y: -100,
+      }
+    }
+    transition={
+      {
+        duration: 0.2
+      }
+    }
+  >
+    {children}
+  </motion.div>
+);
 
-import GoToTop from './components/common/GoToTop'
+const AnimatedRoutes = () => {
+  const location = useLocation();
+
+  return (
+    <AnimatePresence mode="wait">
+      <Routes location={location} key={location.pathname}>
+        <Route path="/" exact element={<PageWrapper><Home /></PageWrapper>} />
+        <Route path="/sudoku" exact element={<PageWrapper><Sudoku /></PageWrapper>} />
+        <Route path="/api" exact element={<PageWrapper><Api /></PageWrapper>} />
+        <Route path="/how-to-use" exact element={<PageWrapper><HowToUse /></PageWrapper>} />
+        <Route path="*" exact element={<PageWrapper><Err404 /></PageWrapper>} />
+      </Routes>
+    </AnimatePresence>
+  );
+};
 
 function App() {
-
-  return <>
-    <BrowserRouter  basename={'/'}>
-      <MainLayout onContextMenu={e => e.preventDefault()} onMouseDown={e => e.preventDefault()} >
-        <Routes>
-          <Route path="/" exact element={<Home />} />
-          <Route path="/sudoku" exact element={<Sudoku />} />
-          <Route path="/api" exact element={<Api />} />
-          <Route path="/how-to-use" exact element={<HowToUse />} />
-          <Route path="*" exact element={<Err404 />} />
-        </Routes>
+  return (
+    <Router basename={'/'}>
+      <MainLayout onContextMenu={e => e.preventDefault()} onMouseDown={e => e.preventDefault()}>
+        <AnimatedRoutes />
         <GoToTop />
       </MainLayout>
-    </BrowserRouter>
-  </>
+    </Router>
+  );
 }
 
 export default App;
